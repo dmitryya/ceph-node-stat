ab7303d3df553abef8224e3aff3f35bbe9c067e8
Dmitry Yatsushkevich <dyatsushkevich@mirantis.com>
2015-02-13 17:14:37 -0800

    ceph_srv_info: ceph_get_pids impl
    Implement ceph_get_pids routine
    Remove class CEPHLocalInfo_

--- ceph_io_lib/ceph_srv_info.py
+++ ceph_io_lib/ceph_srv_info.py
@@ -3,40 +3,49 @@
 import os
 import glob
 import subprocess
+import json
 from os.path import splitext, basename
 
-class CEPHLocalSrvInfo():
-    def __init__(self, adm_socket_path='/var/run/ceph'):
-        self.adm_socket_path=adm_socket_path
+CEPH_SOCKET_PATH = os.getenv("CEPH_RUN_PATH", "/var/run/ceph/")
 
-    def get_pids(self):
-            """Return PIDs as list of dict (name, pid) for all CEPH
-            services on local node.
-            """
-            return [{'name':'osd.0', 'pid':1000}, {'name':'mon.0', 'pid':1001}, {'name':'mds.0', 'pid':1002}]
+def get_ceph_pids():
+    """Return PIDs as list of dict (name, pid) for all CEPH
+    services on local node.
+    """
 
-    def get_disk(self):
-            """Return list of disk devices wich is used by all CEPH services on local node."""
-            return ['/dev/sdx', '/dev/sdy', '/dev/sdz']
+    pids = []
 
-    def _get_srv_list(self):
-            """ Returns list of srv (ceph creatures) on node """
-            srv_list = [splitext(basename(sock))[0] for sock in glob.glob(self.adm_socket_path + "*.asok")]
-            return srv_list
-    
-    def _get_srv_config(self, name):
-            """ Get CEPH Service Config """
-            cmd = "ceph --admin-daemon %s/%s.asok perf %s" % (self.adm_socket_path, name)
-            PIPE = subprocess.PIPE
-            p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
-            return json.loads(p.stdout.read())
+    for srv in get_srv_list():
+        cfg = get_srv_config(srv)
+        pid_file = cfg['pid_file']
+        pid = 0
+        with open(cfg['pid_file'], 'r') as f:
+            pids.append({'name': srv, 'pid': int(f.read())})
+
+    return pids
+
+def get_ceph_disk():
+    """Return list of disk devices wich is used by all CEPH services on local node."""
+    return ['/dev/sdx', '/dev/sdy', '/dev/sdz']
+
+def get_srv_list():
+    """ Returns list of srv (ceph creatures) on node """
+    srv_list = [splitext(basename(sock))[0] for sock in glob.glob(CEPH_SOCKET_PATH + "*.asok")]
+    return srv_list
+
+def get_srv_config(name):
+    """ Get CEPH Service Config """
+    cmd = "ceph --admin-daemon %s/%s.asok config show" % (CEPH_SOCKET_PATH, name)
+    PIPE = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+    #d = p.stdout.read()
+    return json.loads(p.stdout.read())
 
 
 def test():
-    node = CEPHLocalSrvInfo()
-    for srv in node.get_pids():
+    for srv in get_ceph_pids():
         print 'Service %s has pid %d' % (srv['name'], srv['pid'])
-    print (node.get_disk())
+    print (get_ceph_disk())
 
 if __name__ == '__main__':
     test()
