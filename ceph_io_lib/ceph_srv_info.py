1c9942dda860469150f3f455a793fb6e0e099793
Dmitry Yatsushkevich <dyatsushkevich@mirantis.com>
2015-02-17 12:34:08 -0800

    ceph_srv_info: get_ceph_disk implementation
    Implementation for get_ceph_disk routine
    Introduce find_mount_point - extract mount point from provided path
    Introduce get_disk_by_mountpoint - return disk device for mount point.

--- ceph_io_lib/ceph_srv_info.py
+++ ceph_io_lib/ceph_srv_info.py
@@ -4,48 +4,75 @@ import os
 import glob
 import subprocess
 import json
-from os.path import splitext, basename
+import psutil
+from os.path import splitext, basename, realpath
 
 CEPH_SOCKET_PATH = os.getenv("CEPH_RUN_PATH", "/var/run/ceph/")
 
+class CephSrvInfo(object):
+    def __init__(self, name, pid):
+        self.name = name
+        self.pid = pid
+
+
 def get_ceph_pids():
-    """Return PIDs as list of dict (name, pid) for all CEPH
+    """Return PIDs as list of SrvInfo for all CEPH
     services on local node.
     """
-
     pids = []
-
     for srv in get_srv_list():
         cfg = get_srv_config(srv)
-        pid_file = cfg['pid_file']
-        pid = 0
-        with open(cfg['pid_file'], 'r') as f:
-            pids.append({'name': srv, 'pid': int(f.read())})
+        with open(cfg['pid_file'], 'r') as file_fd:
+            pids.append(CephSrvInfo(srv, int(file_fd.read())))
 
     return pids
 
 def get_ceph_disk():
-    """Return list of disk devices wich is used by all CEPH services on local node."""
-    return ['/dev/sdx', '/dev/sdy', '/dev/sdz']
+    """Return list of disk devices wich is used by all
+       CEPH services on local node."""
+    disks = []
+    for srv in get_srv_list():
+        cfg = get_srv_config(srv)
+        for key in ['osd_data', 'osd_journal', 'mds_data', 'mon_data']:
+            mnt_point = cfg[key]
+            disk = get_disk_by_mountpoint(find_mount_point(mnt_point))
+            if disk not in disks:
+                disks.append(disk)
+    return disks
 
 def get_srv_list():
     """ Returns list of srv (ceph creatures) on node """
-    srv_list = [splitext(basename(sock))[0] for sock in glob.glob(CEPH_SOCKET_PATH + "*.asok")]
+    srv_list = [splitext(basename(sock))[0] \
+                for sock in glob.glob(CEPH_SOCKET_PATH + "*.asok")]
     return srv_list
 
 def get_srv_config(name):
     """ Get CEPH Service Config """
-    cmd = "ceph --admin-daemon %s/%s.asok config show" % (CEPH_SOCKET_PATH, name)
-    PIPE = subprocess.PIPE
-    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
-    #d = p.stdout.read()
-    return json.loads(p.stdout.read())
+    cmd = "ceph --admin-daemon %s/%s.asok config show" % \
+        (CEPH_SOCKET_PATH, name)
+    out = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, \
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return json.loads(out.stdout.read())
 
+def get_disk_by_mountpoint(mnt_point):
+    """ Return disk of mountpoint """
+    diskparts = psutil.disk_partitions()
+    for item in diskparts:
+        if item.mountpoint == mnt_point:
+            return realpath(item.device)
+    return None
+
+def find_mount_point(path):
+    """ Find mount point by provided path """
+    path = os.path.abspath(path)
+    while not os.path.ismount(path):
+        path = os.path.dirname(path)
+    return path
 
 def test():
     for srv in get_ceph_pids():
-        print 'Service %s has pid %d' % (srv['name'], srv['pid'])
-    print (get_ceph_disk())
+        print 'Service %s has pid %d' % (srv.name, srv.pid)
+    print get_ceph_disk()
 
 if __name__ == '__main__':
     test()
